6c7d97eea0e5afcff451c0d87b2b7cb1fc597cd4
taiberi <eataiberi@yanao.ru>
2025-12-30 11:32:51 +0500

    Add market

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,11 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel | About</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
-        <div class="container mt-4">
-            <h1>О блоге</h1>
-            <p>Эксперименты с Ларавелем на Хекслете</p>
-            <!-- BEGIN (write your solution here) -->
-            @foreach ($team as $user)
-                <p>This is user {{ $user['name']}} {{ $user['position']}}</p>
-            @endforeach
-            <!-- END -->
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('header', 'О блоге')
+
+@section('content')
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
