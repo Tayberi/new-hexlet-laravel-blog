37a188a681744182debffae458972802e8197d9e
taiberi <eataiberi@yanao.ru>
2026-01-16 15:46:40 +0500

    Fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Article;
 
 
 $team = [
@@ -18,7 +19,7 @@ Route::get('/about', function () use ($team){
 })->name('about');
 
 Route::get('articles', function () {
-    $articles = App\Models\Article::all();
+    $articles = Article::all();
     return view('articles', ['articles' => $articles]);
 });
 
